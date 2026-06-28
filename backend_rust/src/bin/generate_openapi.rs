9aead9b28ac316073a688a83c9504f40d8ea55a1
Javier Arrieta <javier@techdelivery.es>
2026-06-28 21:11:42 +0000

    cleanup

--- backend_rust/src/bin/generate_openapi.rs
+++
@@ -1,8 +0,0 @@
-use backend_rust::openapi::ApiDoc;
-use utoipa::OpenApi;
-
-fn main() {
-    let doc = ApiDoc::openapi();
-    let json = serde_json::to_string_pretty(&doc).expect("Failed to serialize OpenAPI spec");
-    println!("{}", json);
-}
